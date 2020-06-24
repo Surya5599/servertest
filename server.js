7868104f15abfc6ab40415b745e948fc224e8e95
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2020-06-23 20:14:29 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("sever is running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log('new Connection: ' + socket.id);
+  socket.on('mouse', mouseMsg);
+
+  function mouseMsg(data) {
+    socket.broadcast.emit('mouse', data);
+    //for all//io.sockets.emit('mouse', data);
+    console.log(data);
+  }
+}
